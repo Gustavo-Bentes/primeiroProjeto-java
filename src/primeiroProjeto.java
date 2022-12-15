5c0c3a5a858b3c4494be9657afe86d8c7a548bf9
Gustavo-Bentes <gustavobentes05@gmail.com>
2022-12-15 16:25:17 -0300

    first commit

---
+++ src/primeiroProjeto.java
@@ -0,0 +1,21 @@
+public class primeiroProjeto {
+    //meu primeiros passos de java 
+    // armazenar propriedades e variaveis 
+    public static void main(String[] args){
+        System.out.println(2 * 8);
+        System.out.println("olá mundo");
+        System.out.println(10 / 5 );
+} 
+        void minhaInstruçãoDeImprimir() {
+
+        }
+
+}
+// ponto de entradas-> entry-point -> ponto que todos os sistemas de programa
+// procura no seu projeto para começar iniciar o projet
+// instrução chamada de 'System.out.println("")'
+// propriedade/variaveis -> onde armazena dados para manipular        
+
+    
+    
+
---
+++ src/variaveis.java
@@ -0,0 +1,45 @@
+
+
+     public class variaveis {
+
+
+    public static void main(String[] args){
+      
+
+     int number  = 10;    
+     double percent = 29.76; 
+     boolean isDriver = true; // representação boolena (verdadeiro|falso)
+       
+     var login = "olá";
+     login = "mundo"; 
+      var password = "kdjakjldjks";
+       
+         
+      number = 30;
+        
+        System.out.println(number); 
+        System.out.println(percent);
+        System.out.println(isDriver);
+        System.out.println(login);
+        System.out.println(password);
+        System.out.println(number + 50);
+    }
+    
+       
+// variaveis -> textos (Strings)
+// variaveis -> numeros (Int)
+// variaveis -> numeros fracionados (Double)
+// variaveis -> booleanas (verdadeiro| falso)
+
+// tipos de dados 
+//
+
+   //int number; declarando variaveis  
+
+
+
+
+}
+
+
+   
